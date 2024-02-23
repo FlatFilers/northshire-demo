8e1a3c47c74d7f47290ae92aa9a9ba4f2e62af61
Merit <merit@flatfile.io>
2024-02-23 11:05:22 -0800

    Use space configure plugin

--- steps/5-workbook.js
+++ steps/5-workbook.js
@@ -1,6 +1,6 @@
 const namespace = ['space:bookstore']
-import api from '@flatfile/api'
 import { blueprint } from '../blueprint'
+import { configureSpace } from "@flatfile/plugin-space-configure"
 import { zipExtractorPlugin } from '@flatfile/plugin-zip-extractor'
 import { xlsxExtractorPlugin } from '@flatfile/plugin-xlsx-extractor'
 import { automap } from '@flatfile/plugin-automap'
@@ -10,41 +10,13 @@ import { exportWorkbookPlugin } from '@flatfile/plugin-export-workbook'
 
 export default function flatfileEventListener(listener) {
   listener.namespace(namespace, (namespacedEvents) => {
-    namespacedEvents.filter({ job: 'space:configure' }, (configure) => {
-      configure.on(
-        'job:ready',
-        async ({ context: { spaceId, environmentId, jobId } }) => {
-          try {
-            await api.jobs.ack(jobId, {
-              info: 'Creating Space',
-              progress: 10,
-            })
-
-            await api.workbooks.create({
-              spaceId,
-              environmentId,
-              ...blueprint,
-            })
-
-            await api.jobs.complete(jobId, {
-              outcome: {
-                message: 'Space Created',
-                acknowledge: true,
-              },
-            })
-              
-          } catch (error) {
-            await api.jobs.fail(jobId, {
-              outcome: {
-                message:
-                  'Space Creation Failed. See Event Logs',
-                acknowledge: true,
-              },
-            })
-          }
-        }
-      )
-    })
+    
+    namespacedEvents.use(configureSpace(
+      { workbooks: [blueprint] },
+      async (event, workbookIds, tick,) => {
+        await tick(80, "Creating Space")
+      }
+    ))
 
     namespacedEvents.use(zipExtractorPlugin())
     namespacedEvents.use(xlsxExtractorPlugin())
@@ -59,7 +31,7 @@ export default function flatfileEventListener(listener) {
         }
       })
     )
-    
+
     namespacedEvents.use(
       recordHook('inventory', (record) => {
         const isbn = record.get('ISBN')
@@ -72,6 +44,5 @@ export default function flatfileEventListener(listener) {
     )
 
     namespacedEvents.use(exportWorkbookPlugin())
-
   })
 }
--- steps/6-theme.js
+++ steps/6-theme.js
@@ -1,69 +1,41 @@
 const namespace = ['space:bookstore']
-import api from '@flatfile/api'
 import { blueprint } from '../blueprint'
+import { configureSpace } from "@flatfile/plugin-space-configure"
 import { zipExtractorPlugin } from '@flatfile/plugin-zip-extractor'
 import { xlsxExtractorPlugin } from '@flatfile/plugin-xlsx-extractor'
 import { automap } from '@flatfile/plugin-automap'
 import { recordHook } from '@flatfile/plugin-record-hook'
+
 import { exportWorkbookPlugin } from '@flatfile/plugin-export-workbook'
 
 export default function flatfileEventListener(listener) {
   listener.namespace(namespace, (namespacedEvents) => {
-    namespacedEvents.filter({ job: 'space:configure' }, (configure) => {
-      configure.on(
-        'job:ready',
-        async ({ context: { spaceId, environmentId, jobId } }) => {
-          try {
-            await api.jobs.ack(jobId, {
-              info: 'Creating Space',
-              progress: 10,
-            })
-
-            await api.workbooks.create({
-              spaceId,
-              environmentId,
-              ...blueprint,
-            })
-
-            const spaceUpdateParams = {
-              metadata: {
-                theme: {
-                  root: {
-                    primaryColor: "#1f3270",
-                  },
-                  sidebar: {
-                    backgroundColor: "#1f3270",
-                    titleColor: "#ac8652",
-                    focusBgColor: "#ac8652",
-                    focusTextColor: "white",
-                    textColor: "white",
-                    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTypXZ37AlxraOpT5pZ8ELdCyxNHBfO2tPuA&usqp=CAU",
-                  },
-                },
+    
+    namespacedEvents.use(configureSpace(
+      { 
+        workbooks: [blueprint],
+        space: {
+          metadata: {
+            theme: {
+              root: {
+                primaryColor: "#1f3270",
               },
-            };
-  
-            await api.spaces.update(spaceId, spaceUpdateParams);
-  
-            await api.jobs.complete(jobId, {
-              outcome: {
-                message: 'Space Created',
-                acknowledge: true,
+              sidebar: {
+                backgroundColor: "#1f3270",
+                titleColor: "#ac8652",
+                focusBgColor: "#ac8652",
+                focusTextColor: "white",
+                textColor: "white",
+                logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTypXZ37AlxraOpT5pZ8ELdCyxNHBfO2tPuA&usqp=CAU",
               },
-            })
-              
-          } catch (error) {
-            await api.jobs.fail(jobId, {
-              outcome: {
-                message:
-                  'Space Creation Failed. See Event Logs',
-                acknowledge: true,
-              },
-            })
-          }
+            },
+          },
         }
-      )
-    })
+      },
+      async (event, workbookIds, tick,) => {
+        await tick(80, "Creating Space")
+      }
+    ))
 
     namespacedEvents.use(zipExtractorPlugin())
     namespacedEvents.use(xlsxExtractorPlugin())
@@ -78,7 +50,7 @@ export default function flatfileEventListener(listener) {
         }
       })
     )
-    
+
     namespacedEvents.use(
       recordHook('inventory', (record) => {
         const isbn = record.get('ISBN')
@@ -91,6 +63,5 @@ export default function flatfileEventListener(listener) {
     )
 
     namespacedEvents.use(exportWorkbookPlugin())
-
   })
 }
